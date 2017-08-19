14562b6bce4d5d2fd3e4a32353749f048942f7cb
PanicLab <PanicLabLimited@gmail.com>
2017-08-19 11:40:14 +0400

    таблица создана и работает

--- src/main/java/com/github/paniclab/services/CreateSchemaServiceImpl.java
+++ src/main/java/com/github/paniclab/services/CreateSchemaServiceImpl.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Logger;
 
 public class CreateSchemaServiceImpl implements CreateSchemaService {
@@ -22,7 +23,7 @@ public class CreateSchemaServiceImpl implements CreateSchemaService {
 
     @Override
     public boolean createSchema() {
-        Path path = Paths.get("/resources/sql/createSchema.sql");
+/*        Path path = Paths.get("/resources/sql/createSchema.sql");
         path = toAbsolute(path);
         try {
             RunScript.execute(connection, Files.newBufferedReader(path));
@@ -30,9 +31,21 @@ public class CreateSchemaServiceImpl implements CreateSchemaService {
             LOGGER.info("Sql script createSchema executed successfully");
         } catch (SQLException | IOException e) {
             e.printStackTrace();
-        }
+        }*/
 
-        return true;
+        String sql;
+        try (Statement statement = connection.createStatement()) {
+            sql = "CREATE TABLE IF NOT EXISTS USERS (ID BIGINT AUTO_INCREMENT, NAME VARCHAR(255) NOT NULL)";
+            statement.executeUpdate(sql);
+
+            sql = "ALTER TABLE USERS ADD CONSTRAINT IF NOT EXISTS users_pk PRIMARY KEY(ID)";
+            statement.executeUpdate(sql);
+            LOGGER.info("Sql script createSchema executed successfully");
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     private Path toAbsolute(Path path) {
@@ -51,7 +64,7 @@ public class CreateSchemaServiceImpl implements CreateSchemaService {
 
     @Override
     public boolean dropSchema() {
-        Path path = Paths.get("/resources/sql/dropSchema.sql").toAbsolutePath();
+/*        Path path = Paths.get("/resources/sql/dropSchema.sql").toAbsolutePath();
         path = toAbsolute(path);
         try {
             RunScript.execute(connection, Files.newBufferedReader(path));
@@ -61,7 +74,18 @@ public class CreateSchemaServiceImpl implements CreateSchemaService {
         }
 
         LOGGER.info("Sql script dropSchema executed successfully");
-        return true;
+        return true;*/
+
+        String sql;
+        try (Statement statement = connection.createStatement()) {
+            sql = "DROP TABLE IF EXISTS USERS";
+            statement.executeUpdate(sql);
+            LOGGER.info("Sql script createSchema executed successfully");
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
