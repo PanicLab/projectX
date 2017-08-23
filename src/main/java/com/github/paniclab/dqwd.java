59be95fcd8570ae5a6ebb782994987fca758c61b
root <secret>
2017-08-23 14:20:21 +0400

    в таблицу добавлены столбцы пароля и соли изменен скрипт решена проблема с несовпадением пароля к БД

--- src/main/java/com/github/paniclab/dqwd.java
+++ src/main/java/com/github/paniclab/dqwd.java
@@ -1,4 +1,48 @@
 package com.github.paniclab;
 
+import com.github.paniclab.utils.PasswordUtil;
+
+import java.io.PrintStream;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class dqwd {
+    public static void main(String[] args) {
+        PasswordUtil util = new PasswordUtil();
+        List<String> passwords = new ArrayList<>(10);
+        passwords.add("1111");
+        passwords.add("А роза упала на лапу Азора");
+        passwords.add("Top Secret");
+        passwords.add("love");
+        passwords.add("qwerty");
+
+        final PrintStream OUT = System.out;
+        for (String pass : passwords) {
+            OUT.print("Пароль: ");
+            OUT.println(pass);
+
+            String safePass = "n/a";
+            String hashPass = "n/a";
+            String salt = "n/a";
+            try {
+                hashPass = util.getHashed(pass);
+                salt = util.salt();
+                safePass = util.getSafe(pass);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            OUT.print("Херишрованный: ");
+            OUT.println(hashPass);
+            OUT.print("Безопасный: ");
+            OUT.println(safePass);
+            OUT.print("Соль: ");
+            OUT.println(salt);
+
+            OUT.println("Длина пароля=" + pass.length());
+            OUT.println("Длина хешированного=" + hashPass.length());
+            OUT.println("Длина безопасного=" + safePass.length());
+            OUT.println("Длина соли=" + salt.length());
+        }
+    }
 }
