cec4b2a9e93de132a33f7b38e7bdcdf7a182fc50
root <secret>
2017-09-01 10:39:24 +0400

    Броблема с закрытием БД и дерегистрацией драйвера решена

--- src/main/java/com/github/paniclab/listeners/DataSourceInitListener.java
+++ src/main/java/com/github/paniclab/listeners/DataSourceInitListener.java
@@ -12,53 +12,41 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Enumeration;
 import java.util.logging.Logger;
 
-public class DataSourceInitListener implements ServletContextListener,
-        HttpSessionListener {
+public class DataSourceInitListener implements ServletContextListener, HttpSessionListener {
 
     private static final Logger LOGGER = Logger.getAnonymousLogger();
 
-    // Public constructor is required by servlet spec
     public DataSourceInitListener() {
     }
 
-    // -------------------------------------------------------
-    // ServletContextListener implementation
-    // -------------------------------------------------------
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        /* This method is called when the servlet context is
-            initialized(when the Web application is deployed).
-             You can initialize servlet context related data here.
-        */
         ServletContext context = sce.getServletContext();
 
-        final String BASE_DIR = context.getRealPath("/");
+        buildJdbcConnectionPool(context);
+        CreateSchemaService.create(context).createSchema();
+    }
+
+    private void buildJdbcConnectionPool(ServletContext cxt) {
+        final String BASE_DIR = cxt.getRealPath("/");
         System.out.println("Базовая: " + BASE_DIR);
-        final String DB_RELATIVE_PATH = context.getInitParameter("db.relative_path");
+        final String DB_RELATIVE_PATH = cxt.getInitParameter("db.relative_path");
         final String URL = "jdbc:h2:" + BASE_DIR + DB_RELATIVE_PATH;
         System.out.println("Вот такая итоговая URL базы данных: " + URL);
-        final String USER = context.getInitParameter("db.user");
-        final String PASS = context.getInitParameter("db.password");
-        JdbcConnectionPool pool = JdbcConnectionPool.create(URL,USER, PASS);
-        context.setAttribute("connection_pool", pool);
+        final String USER = cxt.getInitParameter("db.user");
+        final String PASS = cxt.getInitParameter("db.password");
+        JdbcConnectionPool pool = JdbcConnectionPool.create(URL, USER, PASS);
+        cxt.setAttribute("connection_pool", pool);
         LOGGER.info("Объект JdbcConnectionPool создан.");
         LOGGER.info("URL базы данных: " + URL);
         LOGGER.info("Логин пользователя: " + USER);
         LOGGER.info("Пароль пользователя: " + PASS);
-
-        CreateSchemaService.create(context).createSchema();
-
-/*        try (CreateSchemaService schemaService = CreateSchemaService.create(pool.getConnection())) {
-            schemaService.createSchema();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
     }
-
 
     private DataSource getDataSource() {
         try {
@@ -72,34 +60,48 @@ public class DataSourceInitListener implements ServletContextListener,
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-      /* This method is invoked when the Servlet Context 
-         (the Web application) is undeployed or 
-         Application Server shuts down.
-      */
-        JdbcConnectionPool pool = (JdbcConnectionPool)sce.getServletContext().getAttribute("connection_pool");
+        ServletContext context = sce.getServletContext();
 
-        //TODO clean from production code ------------>
-/*        try (CreateSchemaService schemaService = CreateSchemaService.create(pool.getConnection())) {
-            schemaService.dropSchema();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
-        // ------------------------------------------->
-
-        if(pool != null) {
-            pool.dispose();
-            pool = null;
-        }
-
-        LOGGER.info("Context destroyed.");
+        closeDatabase(context);
+        deregisterJdbcDrivers();
+        LOGGER.info("Контекст приложения закрыт.");
     }
 
-    // -------------------------------------------------------
-    // HttpSessionListener implementation
-    // -------------------------------------------------------
+    private void closeDatabase(ServletContext context) {
+        JdbcConnectionPool pool = (JdbcConnectionPool)context.getAttribute("connection_pool");
+        if(pool != null) {
+            try (Connection connection = pool.getConnection();){
+                Statement statement = connection.createStatement();
+                statement.executeUpdate("SHUTDOWN ");
+                LOGGER.info("База данных успешно закрыта.");
+            } catch (SQLException e) {
+                LOGGER.warning("Не удалось закрыть базу данных.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void deregisterJdbcDrivers() {
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            deregisterDriver(drivers.nextElement());
+        }
+    }
+
+    private void deregisterDriver(Driver driver) {
+        try {
+            DriverManager.deregisterDriver(driver);
+            LOGGER.info("Драйвер " + driver + " дерегестрирован успешно.");
+        } catch (SQLException e) {
+            LOGGER.warning("Не удалось дерегестрировать драйвер " + driver);
+            e.printStackTrace();
+        }
+    }
+
+
+
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-        /* Session is created. */
         JdbcConnectionPool pool =
                 (JdbcConnectionPool) se.getSession().getServletContext().getAttribute("connection_pool");
         try {
@@ -112,7 +114,6 @@ public class DataSourceInitListener implements ServletContextListener,
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        /* Session is destroyed. */
         Connection connection = (Connection)se.getSession().getAttribute("connection");
         if(connection != null) {
             try {
